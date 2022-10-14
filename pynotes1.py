d681c479a3f4c1b71f273e8547ec885b31f6d990
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-10-14 20:05:04 +0530

    notes1

---
+++ pynotes1.py
@@ -0,0 +1,14 @@
+# varable in python is declared without keywords
+ 
+
+# Data Types
+# - Variable can store data of different types and can also do different 
+#   things.
+# - Python has following data types.
+# - TextType:str
+# - Numberic Types: int,float,complex
+# - sequence Types: list,tuple,range
+# - Mapping Types: dict
+# - Set Types: set,frozenset
+# - Boolean Types : bool
+# - Binary Types : bytes,bytearray,memoryview
